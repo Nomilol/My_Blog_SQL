847612990d9fa263d952b77a39843c35270f54cc
Axel <lezinaxel@gmail.com>
2017-02-01 15:20:59 +0100

    Affichage d'un message d'erreur si un champ n'est pas rempli

---
+++ submit_post.php
@@ -0,0 +1,10 @@
+<?php
+require 'form.php';
+
+if(empty($_POST['title'])) {
+  echo 'error, a field is empty';
+}elseif (empty($_POST['title'])) {
+  echo 'error, a field is empty';
+}elseif (empty($_POST['title'])) {
+  echo 'error, a field is empty';
+}
