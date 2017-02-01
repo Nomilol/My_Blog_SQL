3428a371fe82d85c7b505a7844fcd38762c95f4d
Axel <lezinaxel@gmail.com>
2017-02-01 11:52:04 +0100

    Mise en place du formulaire pour créer un nouvel article

---
+++ form.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Formulaire</title>
+  </head>
+  <body>
+    <form class="" action="index.php" method="post">
+      <div class="titleInput">
+        <label for="title">Titre</label>
+        <input type="text" name="title" value="">
+    </div>
+    <div class="authorInput">
+      <label for="author">Auteur</label>
+      <input type="text" name="author" value="">
+    </div>
+    <div class="contentInput">
+      <label for="content">Article</label>
+      <textarea name="content" rows="8" cols="80"></textarea>
+    </div>
+    <button type="submit" name="button">Envoyer</button>
+    </form>
+  </body>
+</html>
