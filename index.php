d8cf6d9b9d11036d1c8c133daebea5669b390cb1
Axel <lezinaxel@gmail.com>
2017-02-01 10:05:33 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php require 'vendor/autoload.php' ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>My Blog</title>
+  </head>
+  <body>
+    <h1>Mon blog</h1>
+  </body>
+</html>
